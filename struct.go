818a34f64a781c63ee9c3989aebc45133189586d
rizkitirta <rizkibuwono0@gmail.com>
2022-05-27 22:27:17 +0700

    Struct Method

--- struct.go
+++ struct.go
@@ -8,6 +8,16 @@ type Customer struct {
 	age  int
 }
 
+// struct method versi 1
+func sayHello(customer Customer, name string) {
+	fmt.Println("Hallo", name, "saya", customer.name)
+}
+
+// struct method versi 2
+func (customer Customer) sayHello2(name string) {
+	fmt.Println("Kenalin", name, "nama saya", customer.name)
+}
+
 func main() {
 	// versi 1
 	var user_1 Customer
@@ -27,6 +37,14 @@ func main() {
 	fmt.Println(user_2)
 
 	// versi 3
-	user_3 := Customer{"arif","kuningan",18}
+	user_3 := Customer{"arif", "kuningan", 18}
 	fmt.Println(user_3)
+
+	// call struct method v1
+	sayHello(user_1, user_2.name)
+	sayHello(user_2, user_3.name)
+
+	// call struct method v2
+	user_1.sayHello2(user_2.name)
+	user_3.sayHello2(user_1.name)
 }
